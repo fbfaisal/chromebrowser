9ce91b606d5a1f0ca256bc1f18d77d74c6c6fb9d
fbfaisal <fbfaisal.qa@gmail.com>
2021-03-23 19:55:47 -0400

    third commit

--- chromebrowser/src/main/java/dropdowntest/Dropdown3.java
+++ chromebrowser/src/main/java/dropdowntest/Dropdown3.java
@@ -1,4 +1,4 @@
-package locators;
+package dropdowntest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,10 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class FacebookSignup {
+public class Dropdown3 {
 
+	
 	@Test
-	public void signup() throws InterruptedException {
+public void signup() throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\fbfai\\Desktop\\D\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
@@ -24,19 +25,16 @@ public class FacebookSignup {
 		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("ahamd");
 		
 		WebElement birth_m=driver.findElement(By.xpath("//select[@id='month']"));
+		
 		Select b_month=new Select(birth_m);
-		b_month.selectByIndex(9);
 		
-		WebElement birth_d=driver.findElement(By.xpath("//select[@name='birthday_day']"));
-		Select b_day=new Select(birth_d);
-		b_day.selectByVisibleText("12");
+		WebElement n_month=b_month.getFirstSelectedOption();
+		System.out.println(n_month.getText());
 		
-		WebElement birth_y=driver.findElement(By.xpath("//select[@id='year']"));
-		Select b_year=new Select(birth_y);
-		b_year.selectByVisibleText("1983");
+		b_month.selectByIndex(8);
 		
-		driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+		WebElement s_month=b_month.getFirstSelectedOption();
+		System.out.println(s_month.getText());
 		
-	}
-	
+}
 }
